5dfeb130a6570d8e1415f1ab08ee595ac930c82f
lshep <shepherl@gmail.com>
2018-06-08 15:30:49 -0400

    Add Bioconductor logo

--- org/bioconductor/bioc_settings.py
+++ org/bioconductor/bioc_settings.py
@@ -4,12 +4,12 @@ from biostar.settings.base import *
 # Override settings as needed
 
 # List all extra template paths here.
-EXTRA_PATHS = [
+EXTRA_TEMPDIR = [
     abspath(HOME_DIR, 'org', 'bioconductor', 'templates')
 ]
 
 # Activate the new PATHS
-TEMPLATE_DIRS = EXTRA_PATHS + list(TEMPLATE_DIRS)
+TEMPLATE_DIRS = EXTRA_TEMPDIR + list(TEMPLATE_DIRS)
 
 # This is to show you what the template loading order is:
 
@@ -28,3 +28,11 @@ END_CATEGORIES = [
 ]
 
 CATEGORIES = START_CATEGORIES + NAVBAR_TAGS + END_CATEGORIES
+
+SITE_LOGO = "bioconductor_logo_rgb_small.jpg"
+
+EXTRA_STATIC = [
+    abspath(HOME_DIR, 'org', 'bioconductor', 'static')
+]
+
+STATICFILES_DIRS = EXTRA_STATIC + list(STATICFILES_DIRS)

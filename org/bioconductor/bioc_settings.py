76a701b79004eb1349a76d487aaee2b790a0c4da
lshep <shepherl@gmail.com>
2018-06-11 10:11:15 -0400

    Update bioc_settings file with some additional comments and documentation

--- org/bioconductor/bioc_settings.py
+++ org/bioconductor/bioc_settings.py
@@ -1,22 +1,22 @@
+#
+# Django settings for support.bioconductor project.
+#   for defaults see  
+#   support.bioconductor.org/biostar/settings/base.py
+#   this script overrides default settings
+#
+
+
+#
 # Load up all known settings
+#
 from biostar.settings.base import *
 
+#
 # Override settings as needed
+#
 
 
-# List all extra template paths here.
-EXTRA_TEMPDIR = [
-    abspath(HOME_DIR, 'org', 'bioconductor', 'templates')
-]
-
-# Activate the new PATHS
-TEMPLATE_DIRS = EXTRA_TEMPDIR + list(TEMPLATE_DIRS)
-
-# This is to show you what the template loading order is:
-print("*** Template loading order")
-for path in TEMPLATE_DIRS:
-    print(path)
-
+# Define Categories in top banner
 START_CATEGORIES = [
     "Latest", "News", "Jobs", "Tutorials"
 ]
@@ -28,15 +28,40 @@ END_CATEGORIES = []
 # These are the tags that always show up in the tag recommendation dropdown.
 POST_TAG_LIST = NAVBAR_TAGS + ["software error"]
 
+# This will form the navbar
 CATEGORIES = START_CATEGORIES + NAVBAR_TAGS + END_CATEGORIES
 
-SITE_LOGO = "bioconductor_logo_rgb_small.jpg"
+# This will appear as a top banner.
+# It should point to a template that will be included.
 
+TOP_BANNER = ""
+#TOP_BANNER = "test-banner.html"
+
+# List all extra static paths here.
 EXTRA_STATIC = [
     abspath(HOME_DIR, 'org', 'bioconductor', 'static')
 ]
 
+# Activate the new static path
 STATICFILES_DIRS = EXTRA_STATIC + list(STATICFILES_DIRS)
+
+
+# List all extra template paths here.
+EXTRA_TEMPDIR = [
+    abspath(HOME_DIR, 'org', 'bioconductor', 'templates')
+]
+
+# Activate the new template path
+TEMPLATE_DIRS = EXTRA_TEMPDIR + list(TEMPLATE_DIRS)
+
+# Debugging
+# This is to show you what the template loading order is:
+print("*** Template loading order")
+for path in TEMPLATE_DIRS:
+    print(path)
+
+# The site logo image on top navigation bar
+SITE_LOGO = "bioconductor_logo_rgb_small.jpg"
 
 # How many recent objects to show in the sidebar.
 RECENT_VOTE_COUNT = 5

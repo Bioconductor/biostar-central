782780a1214f620a58cc4dad76f73ad466a75f7d
lshep <shepherl@gmail.com>
2018-06-11 08:55:51 -0400

    Limit side bar to 5 entries

--- org/bioconductor/bioc_settings.py
+++ org/bioconductor/bioc_settings.py
@@ -3,6 +3,7 @@ from biostar.settings.base import *
 
 # Override settings as needed
 
+
 # List all extra template paths here.
 EXTRA_TEMPDIR = [
     abspath(HOME_DIR, 'org', 'bioconductor', 'templates')
@@ -12,7 +13,6 @@ EXTRA_TEMPDIR = [
 TEMPLATE_DIRS = EXTRA_TEMPDIR + list(TEMPLATE_DIRS)
 
 # This is to show you what the template loading order is:
-
 print("*** Template loading order")
 for path in TEMPLATE_DIRS:
     print(path)
@@ -25,6 +25,9 @@ NAVBAR_TAGS = []
 
 END_CATEGORIES = []
 
+# These are the tags that always show up in the tag recommendation dropdown.
+POST_TAG_LIST = NAVBAR_TAGS + ["software error"]
+
 CATEGORIES = START_CATEGORIES + NAVBAR_TAGS + END_CATEGORIES
 
 SITE_LOGO = "bioconductor_logo_rgb_small.jpg"
@@ -35,3 +38,7 @@ EXTRA_STATIC = [
 
 STATICFILES_DIRS = EXTRA_STATIC + list(STATICFILES_DIRS)
 
+# How many recent objects to show in the sidebar.
+RECENT_VOTE_COUNT = 5
+RECENT_USER_COUNT = 5
+RECENT_POST_COUNT = 5

3b350a0d59a4485be3e2e4b0b9ed89d7fcce2380
lshep <lori.shepherd@roswellpark.org>
2019-01-17 09:58:43 -0500

    Update celery scripts for Bioconductor environment and paths

--- conf/server/celery.beat.sh
+++ conf/server/celery.beat.sh
@@ -2,16 +2,16 @@
 set -ue
 
 # This is required so that the default configuration file works.
-source /home/www/sites/biostar-central/live/deploy.env
+source /home/www-data/biostar-central/live/deploy.env
 
 # Location of the log file
-LOGFILE=/home/www/sites/biostar-central/live/logs/celery-beat.log
+LOGFILE=/home/www-data/biostar-central/live/logs/celery-beat.log
 
 # The name of the application.
 APP="biostar"
 
 # The gunicorn instance to run.
-CELERY="/home/www/.virtualenvs/biostar/bin/celery"
+CELERY="/home/www-data/.virtualenvs/biostarsenv/bin/celery"
 
 echo "starting celery beat with DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE"
 
